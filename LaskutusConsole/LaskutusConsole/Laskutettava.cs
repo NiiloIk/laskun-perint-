a17bf16d1a8be6f0b992abc345ffd854a311ec50
labran peruskäyttäjä <tllabra@labra.local>
2022-09-21 11:22:42 +0300

    console ohjelma lisätty, jossa alkuvaihe koodista.

---
+++ LaskutusConsole/LaskutusConsole/Laskutettava.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography.X509Certificates;
+
+internal class Vastaanottaja
+{
+    string etunimi;
+    string sukunimi;
+    string osoite;
+    string puhNo;
+    string sposti;
+
+    public Vastaanottaja(string Etunimi, string Sukunimi, string Osoite, string PuhNo, string Sposti)
+    {
+        etunimi = Etunimi;
+        sukunimi = Sukunimi;
+        osoite = Osoite;
+        puhNo = PuhNo;
+        sposti = Sposti;
+    }
+    public string Nimi()
+    {
+        return ($"{etunimi} {sukunimi}");
+    }
+    public string Info()
+    {
+        return ($"Nimi: {etunimi} {sukunimi}\nOsoite: {osoite}\nPuhelin numero: {puhNo}\nSähköposti: {sposti}");
+    }
+}
---
+++ LaskutusConsole/LaskutusConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using LaskutusConsole;
+
+var matti = new Vastaanottaja("Matti", "Mattinen", "Kotikatu 12", "044123456", "sposti@gmail.fi");
+
+var mattilasku = new AsetaLasku(125.45F, matti);
+
+Console.WriteLine(mattilasku.laskuInfo());
+
+// Console.WriteLine(matti.Info());
+

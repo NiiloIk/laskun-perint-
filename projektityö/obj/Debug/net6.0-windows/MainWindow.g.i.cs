fb820c8055ec166c410a79cd9cd4d827ee715a48
labran peruskäyttäjä <tllabra@labra.local>
2022-09-26 11:29:48 +0300

    ikkunoita

--- projektityö/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ projektityö/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace projektityö {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/projektityö;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/projektityö;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);

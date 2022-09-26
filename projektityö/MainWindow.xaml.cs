950eee28bff9bcad061903b0f569d67062e13027
Joona <joona@a.c>
2022-09-26 11:11:00 +0300

    Lisätty luokkien kokeilua pääohjelmaan

--- projektityö/MainWindow.xaml.cs
+++ projektityö/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using projektityö.Luokat;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,25 @@ namespace projektityö
         public MainWindow()
         {
             InitializeComponent();
+
+            DateOnly pvmTänään = DateOnly.FromDateTime(DateTime.Now);
+
+            Lasku lasku = new Lasku();
+            lasku.summa = 30f;
+
+            Vastaanottaja vastaanottaja = new Vastaanottaja("Matti", "Meikäläinen", "Matintie 1A, 00100 MATTILA");
+
+            lasku.Eräpäivä = pvmTänään.AddDays(-35);
+            lasku.Vastaanottaja = vastaanottaja;
+
+            lasku.Maksumuistutus1.Lähetä();
+            lasku.Maksumuistutus2.Lähetä();
+
+            string tuloste = lasku.Tietotuloste();
+            Debug.Write(tuloste);
+            MessageBox.Show(tuloste);
+
+            //lasku.Maksumuistutus2.lähetä();
         }
     }
 }

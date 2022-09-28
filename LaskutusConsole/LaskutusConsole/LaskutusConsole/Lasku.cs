517dbf441c326792828613aa5db29f2918022120
labran peruskäyttäjä <tllabra@labra.local>
2022-09-28 10:44:07 +0300

    Keksitty järkevä tapa suorittaa laskutietojen tallentaminen

---
+++ LaskutusConsole/LaskutusConsole/LaskutusConsole/Lasku.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaskutusConsole
+{
+    internal class Lasku
+    {
+        // Tarpeelliset tiedot public:ina jotta tiedot tallentuvat.
+        public float summa { get; set; }
+        public Vastaanottaja henkilö { get; set; }
+        public DateTime alkuPvm { get; set; }
+        public DateTime eräpäivä { get; set; }
+        public bool laskuSuoritettu;
+
+        // nämä pitää vielä lisätä jos haluaa.
+
+        float maksettava; // summa + maksulisät
+        DateTime maksuPäivä;
+        //maksulisä lista
+
+        // Tässä constructori joka luo laskun.
+        // ei tarvitse erikseen tehdä AsetaLasku funktiota.
+        public Lasku(float summa, Vastaanottaja henkilö)
+        {
+            this.summa = summa;
+            this.henkilö = henkilö;
+            alkuPvm = DateTime.Now;
+            eräpäivä = alkuPvm.AddDays(14);
+            laskuSuoritettu = false;
+        }
+        // !!Tämän voi käytännössä poistaa!!
+        //public void AsetaLasku(float MääräEuroina, Vastaanottaja Henkilö)
+        //{
+        //    DateTime päivämäärä = DateTime.Now;
+        //    summa = MääräEuroina;
+        //    henkilö = Henkilö;
+        //    alkuPvm = päivämäärä;
+        //    eräpäivä = päivämäärä.AddDays(14);
+        //}
+
+        // Tätäkään ei käytännössä tarvitse tulevaisuudessa
+        //public string laskuTiedot(Vastaanottaja henkilö)
+        //{
+        //    return ($"Määrä euroina: {Maksulisä()}\nHenkilö: {henkilö.etunimi} {henkilö.sukunimi}\n" +
+        //        $"Laskun voimaan astumis päivämäärä: {alkuPvm}\nEräpäivämäärä: {eräpäivä}" +
+        //        $"\nOnko maksettu: {OnkoSuoritettu(laskuSuoritettu)}");
+        //}
+
+        // tämänkin voisi ehkä viedä johonkin toiseen paikkaan jotta tämä
+        // Lasku osio pysyisi yksinkertaisempana ja selkeämpänä
+        private float Maksulisä()
+        {
+            DateTime nyt = DateTime.Now;
+            if (nyt > eräpäivä)
+            {
+                return summa + summa * 0.05F;
+            }
+            else
+            {
+                return summa;
+            }
+        }
+    }
+}
---
+++ LaskutusConsole/LaskutusConsole/LaskutusConsole/Tallentaminen.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Security.Authentication;
+
+namespace LaskutusConsole
+{
+    internal class Tallentaminen
+    {
+        public List<Lasku> Laskut { get; set; }
+        private string tiedostoNimi = "laskutiedot.Json";
+
+        public Tallentaminen()
+        {
+            Laskut = new List<Lasku>();
+        }
+        
+        public void Init()
+        {
+            try
+            {
+                var raakaJson = File.ReadAllText(tiedostoNimi);
+                if (raakaJson.Length > 5)
+                {
+                    var henkilöt = JsonSerializer.Deserialize<Tallentaminen>(raakaJson);
+                    if (henkilöt != null)
+                    {
+                        this.Laskut = henkilöt.Laskut;
+                    }
+                }
+                Console.WriteLine("Tietokannan luku onnistui");
+            }
+            catch
+            {
+                Console.WriteLine("Tietokanta tiedostoa ei voitu lukea");
+            }
+        }
+        public void LisääLasku(Lasku uusiLasku)
+        {
+            Laskut.Add(uusiLasku);
+            
+            TallennaKanta();
+        }
+        public void TallennaKanta()
+        {
+            string json = JsonSerializer.Serialize<Tallentaminen>(this);
+            File.WriteAllText(tiedostoNimi, json);
+        }
+        public void ListaaLaskut()
+
+        {
+
+            foreach (var lasku in Laskut)
+            {
+                string maksettu = (lasku.laskuSuoritettu) ? "On" : "Ei";
+                string text = $"Määrä euroina: {lasku.summa}\nHenkilö: {lasku.henkilö.etunimi} {lasku.henkilö.sukunimi}\n" +
+                    $"Voimaan astumis päivämäärä: {lasku.alkuPvm}\nEräpäivämäärä: {lasku.eräpäivä}\n" +
+                    $"Onko maksettu: {maksettu}";
+                
+                Console.WriteLine(text);
+                Console.WriteLine();
+            }
+
+        }
+
+    }
+}

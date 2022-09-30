72fbd918ab05e88c27f15589b3bcf32570dc971c
Valtteri <valtteri.pohto@student.hamk.fi>
2022-09-30 18:26:09 +0300

    testi listBox win2

--- projektityö/Window2.xaml.cs
+++ projektityö/Window2.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Text.Json;
+using System.IO;
+using projektityö.Luokat;
 
 namespace projektityö
 {
@@ -21,7 +24,27 @@ namespace projektityö
     {
         public Window2()
         {
-            InitializeComponent();
+            // void Init()
+            //{
+            //    try
+            //    {
+            //        var raakaJson = File.ReadAllText(tiedostoNimi);
+            //        if (raakaJson.Length > 5)
+            //        {
+            //            var henkilöt = JsonSerializer.Deserialize<Tallentaminen>(raakaJson);
+            //            if (henkilöt != null)
+            //            {
+            //                this.Laskut = henkilöt.Laskut;
+            //            }
+            //        }
+            //        Console.WriteLine("Tietokannan luku onnistui");
+            //    }
+            //    catch
+            //    {
+            //        Console.WriteLine("Tietokanta tiedostoa ei voitu lukea");
+            //    }
+            //}
+
         }
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
@@ -29,6 +52,11 @@ namespace projektityö
             MainWindow pääikkuna = new MainWindow();
             pääikkuna.Show();
             this.Close();
+        }
+
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //listBox.ItemsSource = Tallentaminen.Laskut;
         }
     }
 }
